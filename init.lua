ffcb0e9661aacd4faf4d82f54f947bfe33710f66
Kshitij Sharma <kshitijs1202@gmail.com>
2025-12-08 01:49:19 +0530

    add flaoting diagonstic kebind

--- init.lua
+++ init.lua
@@ -182,6 +182,9 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>df', function()
+  vim.diagnostic.open_float(nil, { focus = true })
+end, { desc = 'Show diagnostic under cursor' })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 -- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
